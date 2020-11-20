8a9ff39fc07d1533d75bb116aaa6a78cadc01a57
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-20 10:15:19 +0100

    Created register controller

--- functions/controllers/auth.controller.js
+++ functions/controllers/auth.controller.js
@@ -7,3 +7,29 @@ const config = require("../config/auth.config");
 
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
+const db = require("../config/db.config");
+
+exports.register = (req, res) => {
+    // Destructure the request body
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+
+    db.collection("users")
+        .doc(username)
+        .set({
+            username,
+            email,
+            password: bcrypt.hashSync(password, 8)
+        })
+        .then(() => {
+            res.status(200).json({message: "User has been registered successfully!"});
+            return;
+        })
+        .catch(err => {
+            res.status(500).json({Error : err});
+            return;
+        })
+}
