274c9ea43a62e998766644e79351297a852571cf
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-20 10:44:35 +0100

    Created controller for login route

--- functions/controllers/auth.controller.js
+++ functions/controllers/auth.controller.js
@@ -25,11 +25,64 @@ exports.register = (req, res) => {
             password: bcrypt.hashSync(password, 8)
         })
         .then(() => {
-            res.status(200).json({message: "User has been registered successfully!"});
+            res.status(200).json({
+                message: "User has been registered successfully!"
+            });
             return;
         })
         .catch(err => {
-            res.status(500).json({Error : err});
+            res.status(500).json({
+                Error: err
+            });
+            return;
+        })
+}
+
+exports.login = (req, res) => {
+    // Destructure request body
+    const {
+        username,
+        password
+    } = req.body;
+
+    // Check if the User exists
+    db.collection("users")
+        .doc(username)
+        .get()
+        .then(doc => {
+            if (!doc.exists) {
+                res.status(404).json({
+                    message: "User not found!"
+                })
+                return;
+            }
+
+            const passwordIsValid = bcrypt.compareSync(password, doc.data().password);
+
+            if (!passwordIsValid) {
+                res.status(401).json({
+                    message: "Inccorect password!",
+                    accessToken: null
+                });
+                return;
+            }
+
+            var token = jwt.sign({
+                id: username
+            }, config.secret, {
+                expiresIn: 86400 // 24 hours
+            })
+
+            res.status(200).send({
+                username,
+                accessToken: token
+            })
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+            res.status(500).json({
+                Error: err
+            });
             return;
         })
 }
