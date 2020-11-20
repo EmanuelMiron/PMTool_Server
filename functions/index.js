aa3c0c9c368ddc910acf14593c963e93d76141b5
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-20 09:41:31 +0100

    Required the auth routes in index.js and set up firebase to work with express

--- functions/index.js
+++ functions/index.js
@@ -2,3 +2,7 @@ const functions = require('firebase-functions');
 
 const express = require('express');
 const app = express();
+
+require('./routes/auth.routes')(app);
+
+exports.api = functions.https.onRequest(app);
