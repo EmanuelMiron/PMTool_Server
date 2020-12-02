ad4a1b7510979aa2b2656d22fef68193d1e6e2a0
EmanuelMiron <emanuel_miron6@gmail.com>
2020-12-02 20:13:21 +0100

    Installed cors

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,14 @@
 const functions = require('firebase-functions');
+const cors = require('cors');
 
 const express = require('express');
 const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000"
+}
+
+app.use( cors());
 
 require('./routes/auth.routes')(app);
 
